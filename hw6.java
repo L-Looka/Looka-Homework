30388256f1e05223bad6266cdd9f4a3c83ea6a6a
L-Looka <looka@bk.ru>
2015-06-29 12:55:20 +0300

    Change places static and public

--- hw6.java
+++ hw6.java
@@ -1,6 +1,6 @@
 class hw6{
 	
-	static public void main (String [] args) {
+	 public static void main (String [] args) {
 		float TF = Integer.parseInt(args[0]);
 		float TC;
 		TC = (TF - 32) * 5/9;
