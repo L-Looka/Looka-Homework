f8431f2529538e31addc230314663bb969bd9dd9
L-Looka <looka@bk.ru>
2015-06-29 12:15:53 +0300

    HW5

---
+++ HW5.java
@@ -0,0 +1,13 @@
+ class HW5 {
+	 
+	 public static void main (String [] args) {
+		 int a = Integer.parseInt(args[0]);
+		 int b;
+		 b = a*a;
+		 System.out.println ("A in degree 2 is " + b );
+		 a = b*b;
+		 System.out.println ("A in degree 4 is " + a);
+		 b = a*a;
+		 System.out.println ("A in degree 8 is " + b);
+	 }
+ }
