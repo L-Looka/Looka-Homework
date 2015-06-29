dc36923b6b61d35fc1fdb0a0140bf440a3043317
L-Looka <looka@bk.ru>
2015-06-29 13:08:19 +0300

    Home Work 7

---
+++ hw7.java
@@ -0,0 +1,9 @@
+class hw7 {
+	
+	public static void main (String [] args){
+		float TC = Integer.parseInt (args[0]);
+		float TF;
+		TF = TC /5*9 +32;
+		System.out.println (" The temperature is " + TF + " degree Fahrenheit");
+	}
+}
