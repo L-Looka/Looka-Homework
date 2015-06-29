eb4618fe6d91543986fabcdb2e036e48036dcbae
L-Looka <looka@bk.ru>
2015-06-29 13:20:51 +0300

    Change Integer.parseInt to Float.parseFloat

--- hw6.java
+++ hw6.java
@@ -1,7 +1,7 @@
 class hw6{
 	
 	 public static void main (String [] args) {
-		float TF = Integer.parseInt(args[0]);
+		float TF = Float.parseFloat(args[0]);
 		float TC;
 		TC = (TF - 32) * 5/9;
 		System.out.println ("The temperature is  " + TC + " degrees Celsius");
--- hw7.java
+++ hw7.java
@@ -1,7 +1,7 @@
 class hw7 {
 	
 	public static void main (String [] args){
-		float TC = Integer.parseInt (args[0]);
+		float TC = Float.parseFloat (args[0]);
 		float TF;
 		TF = TC /5*9 +32;
 		System.out.println (" The temperature is " + TF + " degree Fahrenheit");
